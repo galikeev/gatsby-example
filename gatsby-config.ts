bc65a47cb4999e296f010942990f511411984b96
Ramil Galikeev <galikeevramil95@gmail.com>
2023-05-01 10:52:09 +0500

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,24 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: ["gatsby-plugin-styled-components",
+  // {
+  //   resolve: 'gatsby-source-graphql',
+  //   options: {
+  //     typeName: 'Products',
+  //     fieldName: 'products',
+  //     url: 'http://localhost:8000/api',
+  //   },
+  // },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "data",
+      path: `${__dirname}/src/data/`,
+    },
+  },
+  "gatsby-transformer-json",
+]
 };
 
 export default config;
